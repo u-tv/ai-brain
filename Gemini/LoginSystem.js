003839346dc377784a7ccb9eb8b25b343b346e31
u-tv <vikkythakur1995@gmail.com>
2026-07-03 23:55:46 +0530

    Delete Gemini directory

--- Gemini/LoginSystem.js
+++
@@ -1,8 +0,0 @@
----
-PROJECT: LoginSystem
-TYPE: Auth
-KEYWORDS: Login,Security,JWT
-MODEL: Gemini
-DATE: 2026-06-19
----
-console.log("Gemini Login Code");
--- Gemini/TestProject.js
+++
@@ -1,8 +0,0 @@
----
-PROJECT: TestProject
-TYPE: Testing
-KEYWORDS: Setup,Working,Done
-MODEL: Gemini
-DATE: 2026-06-19
----
-console.log("System is working perfectly!");
